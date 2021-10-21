e60e502f8b2668a63013cd1846293f40a525084d
Mona Lisa <holmesmainsend@gmail.com>
2021-10-21 17:40:14 -0400

    added local time checker

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 var now = moment();
 var nowFormatted = now.format("dddd" + "," + " MMMM Do");
 $("#currentDay").append(nowFormatted);
+
+
+// Current time checker
+var nowHourMinutes = now.format("LT");
+console.log(nowHourMinutes);
+
+
