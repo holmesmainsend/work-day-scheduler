6e60e2cbc64520dccb3c7fa263a0d1ed8cfc98fb
Mona Lisa <holmesmainsend@gmail.com>
2021-10-21 18:57:23 -0400

    added time difference determiner

--- script.js
+++ script.js
@@ -5,7 +5,47 @@ $("#currentDay").append(nowFormatted);
 
 
 // Current time checker
-var nowHourMinutes = now.format("LT");
-console.log(nowHourMinutes);
+var currentHour = now.format("k");
 
 
+// Time difference determiner
+function timeDeterminer() {
+if (currentHour < 9) {
+    $(".col-10").addClass("future");
+} else if (currentHour < 10 ) {
+    $(".nineAM").addClass("present");
+} else if (currentHour < 11) {
+    $(".nineAM").addClass("past");
+    $(".tenAM").addClass("present");
+} else if (currentHour < 12) {
+    $(".tenAM").addClass("past");
+    $(".elevenAM").addClass("present");
+} else if (currentHour < 13) {
+    $(".elevenAM").addClass("past");
+    $(".twelvePM").addClass("present");
+} else if (currentHour < 14) {
+    $(".twelvePM").addClass("past");
+    $(".onePM").addClass("present");
+} else if (currentHour < 15) {
+    $(".onePM").addClass("past");
+    $(".twoPM").addClass("present");
+} else if (currentHour < 16) {
+    $(".twoPM").addClass("past");
+    $(".threePM").addClass("present");
+} else if (currentHour < 17) {
+    $(".threePM").addClass("past");
+    $(".fourPM").addClass("present");
+} else if (currentHour < 18) {
+    $(".fourPM").addClass("past");
+    $(".fivePM").addClass("present");
+} else if (currentHour >= 18) {
+    $(".col-10").addClass("past");
+}
+};
+
+
+// Time determiner refresher
+function timeRefresher() {
+    setInterval(timeDeterminer, 1000);
+}; 
+timeRefresher();
