4dfa04ec58c488b7596f1d92bd8589406e306050
Mona Lisa <holmesmainsend@gmail.com>
2021-10-23 20:07:30 -0400

    began bug testing

--- script.js
+++ script.js
@@ -1,46 +1,16 @@
-// TODO: fix time slot color class assignments
-
-// Current day
+// Current day display
 var now = moment();
 var nowFormatted = now.format("dddd" + "," + " MMMM Do");
 $("#currentDay").append(nowFormatted);
 
-
 // Schedule notes loader
 for (i = 0; i < localStorage.length; i++) {
 var slotKey = parseInt(localStorage.key(i));
+console.log(slotKey);
 var slotText = localStorage.getItem(slotKey);
+console.log(slotText);
 $("." + slotKey).text(slotText);
 };
-
-
-// Time determiner
-function timeDeterminer() {
-    var now = moment();
-    var currentHour = now.format("k");
-    var currentHourParsed = parseInt(currentHour);
-
-        var x = 19;
-        if (x < currentHourParsed) {
-            console.log("past");
-        } else if (x === currentHourParsed){
-            console.log("present");
-        } else {
-            console.log("future")
-        }
-
-
-
-};
-
-
-timeDeterminer();
-// Time determiner refresher
-// function timeRefresher() {
-//     setInterval(timeDeterminer, 5000);
-// }; 
-// timeRefresher();
-
 
 // Schedule editor
 $(".saveBtn").on("click", function() {
@@ -48,3 +18,32 @@ $(".saveBtn").on("click", function() {
     var key = $(this).siblings()[1].className.slice(19, 21);
     localStorage.setItem(key, text);
 });
+
+// Time difference determiner
+function timeDiff() {
+    var now = moment();
+    var currentHour = now.format("k");
+    var currentHourParsed = parseInt(currentHour);
+
+    for (i = 0; i < 9; i++) {
+    var hourVal = $(".description")[i].className.slice(19, 21);
+    var hourValParsed = parseInt(hourVal);
+
+    if (hourValParsed < currentHourParsed) {
+        $("." + [i + 9]).removeClass("present").removeClass("future").addClass("past");
+
+    } else if (hourValParsed === currentHourParsed) {
+        $("." + [i + 9]).removeClass("past").removeClass("future").addClass("present");
+
+    } else {
+        $("." + [i + 9]).removeClass("past").removeClass("present").addClass("future");
+    }
+    };
+};
+timeDiff();
+
+// Time difference refresher
+function timeRefresher() {
+    setInterval(timeDiff, 5000);
+}; 
+timeRefresher();
